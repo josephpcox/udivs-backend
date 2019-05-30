e882f20fbb19c3bbe2d3825e6498dc5a3f77d48b
John Cameron <jcameron2@mail.usf.edu>
2019-05-30 13:33:21 -0400

    Update udivs.py

--- udivs.py
+++ udivs.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask,render_template
 
 app = Flask(__name__)
@@ -6,4 +7,4 @@ app = Flask(__name__)
 def hello():
     return render_template('hello.html')
 
-app.run(debug=False, host='0.0.0.0', port=environ.get("PORT", 5000))
+app.run(debug=False, host='0.0.0.0', port=os.environ.get("PORT", 5000))
