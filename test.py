1d8d23d29527f8171425528c081a4b9670668570
Joseph Cox <josephcox@Josephs-MacBook-Air.local>
2019-06-12 19:27:10 -0400

    db connection debug

--- test.py
+++ test.py
@@ -7,17 +7,12 @@ def test_users_table():
     print(' * Testing database connection...', file=sys.stderr)
     try:
         DATABASE_URL = os.environ['DATABASE_URL']
-        print(type(DATABASE_URL))
         CONNECTION = psycopg2.connect(DATABASE_URL, sslmode='require')
+        print(CONNECTION.get_dsn_parameters(), file=sys.stderr)
         cursor = CONNECTION.cursor()
-        print(' *' + CONNECTION.get_dsn_parameters() + '\n', file=sys.stderr)
-        # check to se if the table exists with sql let the exception handler catch the error
-        cursor.execute(('CREATE TABLE IF NOT EXISTS users (user_id serial PRIMARY KEY,' +
-                        'username VARCHAR (50) UNIQUE NOT NULL,' +
-                        ' password VARCHAR (50) NOT NULL,' +
-                        'admin BOOLEAN NOT NULL DEFAULT FALSE,' +
-                        'csv_file BYTEA)'))
+        cursor.execute('CREATE TABLE IF NOT EXISTS users (user_id serial PRIMARY KEY,username VARCHAR (50) UNIQUE NOT NULL,password VARCHAR (50) NOT NULL,admin BOOLEAN NOT NULL DEFAULT FALSE,csv_file BYTEA)')
         CONNECTION.commit()  # Need to commit so that changes in db schema can be changed
+        cursor.close()
         return CONNECTION
     except (Exception, psycopg2.Error) as error:
         print('\033[93m'+' * Error while connecting to PostgreSQL' +
