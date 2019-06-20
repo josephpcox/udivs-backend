0b585a2155bf7a5e7685a353bc2e8c14b5d71027
Joseph Cox <josephcox@Josephs-MacBook-Air.local>
2019-06-19 22:38:39 -0400

    push to test email

--- udivs.py
+++ udivs.py
@@ -207,7 +207,7 @@ class Enrolment(Resource):
             sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
             from_email = Email("test@example.com")
             subject = "Hello World from the SendGrid Python Library!"
-            to_email = Email(request_data['email'])
+            to_email = Email(str(request_data['email']))
             content = Content("text/plain", "Hello, Email!")
             mail = Mail(from_email, subject, to_email, content)
             response = sg.client.mail.send.post(request_body=mail.get())
