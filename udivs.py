458be5dab5ab95c3b20082606ed35b70af616091
John Cameron <jcameron2@mail.usf.edu>
2019-06-24 19:52:50 -0400

    Add captcha to register page, implement email verification

--- udivs.py
+++ udivs.py
@@ -1,7 +1,9 @@
 """@author: joseph cox"""
 
 import binascii
+import json
 
+import requests
 import sendgrid
 from flask import Flask, jsonify, render_template, request
 from flask_jwt_extended import (
@@ -22,41 +24,53 @@ jwt = JWTManager(app)
 
 api = Api(app)  # create the api
 
-
 @app.route('/api/register', methods=['POST'])
 def register():
     """Creates a new account"""
     parser = reqparse.RequestParser()
     parser.add_argument('email', required=True, type=str, help='email field is required')
     parser.add_argument('password', required=True, type=str, help='password field is required')
+    parser.add_argument('g-recaptcha-response', required=True, type=str, help='recaptcha response is missing')
+
     request_data = parser.parse_args(strict=True)
 
+    recaptcha_response = request_data['g-recaptcha-response']
     email = request_data['email']  # TODO Validate email formatting
     password = hash_password(request_data['password'])
 
-    db_connection = get_database_connection()
-    cursor = db_connection.cursor()
-    cursor.execute('INSERT INTO users (email,password) VALUES(%s,%s) RETURNING id;', (email, password))
-    user_id = cursor.fetchone()[0]
+    recaptcha_server_response = requests.post('https://www.google.com/recaptcha/api/siteverify',
+                                              {"secret": "6LcFe6oUAAAAALoJ9VBM3xOtOI-CSV70IL6mRJGk",
+                                               "response": recaptcha_response,
+                                               "remoteip": request.remote_addr})
 
-    email_token = binascii.hexlify(os.urandom(20)).decode()
-    cursor.execute('INSERT INTO email_tokens (email,token) VALUES(%s,%s);', (email, email_token))
+    if recaptcha_server_response.status_code == 200 and json.load(recaptcha_server_response)["success"]:
 
-    db_connection.commit()
-    cursor.close()
-    db_connection.close()
+        db_connection = get_database_connection()
+        cursor = db_connection.cursor()
+        cursor.execute('INSERT INTO users (email,password) VALUES(%s,%s) RETURNING id;', (email, password))
+        user_id = cursor.fetchone()[0]
 
-    sg = sendgrid.SendGridAPIClient(os.environ['SENDGRID_API_KEY'])
-    message = Mail(from_email='UDIVS-team@UDIVS.com', to_emails=email,
-                   subject='Verify your email address',
-                   html_content='Please <a href="https://udivs.herokuapp.com/email_verify?token='
-                                + email_token + '">click here</a> to verify your email address')
-    response = sg.send(message)
+        email_token = binascii.hexlify(os.urandom(20)).decode()
+        cursor.execute('INSERT INTO email_tokens (email,token) VALUES(%s,%s);', (email, email_token))
 
-    print('Email sent with status ' + str(response.status_code))
+        db_connection.commit()
+        cursor.close()
+        db_connection.close()
 
-    access_token = create_access_token(identity=user_id)
-    return jsonify(token=access_token), 201
+        sg = sendgrid.SendGridAPIClient(os.environ['SENDGRID_API_KEY'])
+        message = Mail(from_email='UDIVS-team@UDIVS.com', to_emails=email,
+                       subject='Verify your email address',
+                       html_content='Please <a href="https://udivs.herokuapp.com/email_verify?token='
+                                    + email_token + '">click here</a> to verify your email address')
+        response = sg.send(message)
+
+        print('Email sent with status ' + str(response.status_code))
+
+        access_token = create_access_token(identity=user_id)
+        return jsonify(token=access_token), 201
+
+    else:
+        return jsonify({"msg": "captcha is invalid"}), 400
 
 
 # Provide a method to create access tokens. The create_access_token()
@@ -119,9 +133,9 @@ def get_csv():
     db_connection.close()
 
     if row is not None:
-        return row[0], 200
+        return jsonify({"csv_file": row[1]}), 200
     else:
-        return "", 204
+        return jsonify({"csv_file": ""}), 204
 
 
 @app.route('/api/account/csv', methods=['PUT'])
@@ -135,10 +149,41 @@ def update_csv():
     db_connection.close()
     return jsonify({"msg": "CSV File updated"}), 200
 
+
+@app.route('/api/verify_email', methods=['POST'])
+def verify_email():
+    parser = reqparse.RequestParser()
+    parser.add_argument('token', required=True, type=str, help='token field is required')
+    request_data = parser.parse_args(strict=True)
+
+    token = request_data['token']
+
+    db_connection = get_database_connection()
+    cursor = db_connection.cursor()
+    cursor.execute('SELECT email FROM email_tokens WHERE token = (%s)', (token,))
+    row = cursor.fetchone()
+
+    cursor.close()
+
+    if row is None:
+        db_connection.close()
+        return jsonify({"msg": "Invalid Token"}), 400
+    else:
+        cursor = db_connection.cursor()
+        cursor.execute('UPDATE users SET email_verified = TRUE WHERE email = (%s)', (row[0],))
+        cursor.close()
+        db_connection.close()
+        return jsonify({"msg": "Email Verified"}), 200
+
 # web pages
 @app.route('/')
 def home():
     return render_template('enroll.html')
+
+
+@app.route('/email_verify')
+def email_verify():
+    return render_template('email_verify.html')
 
 if __name__ == "__main__":
     # Initialize the database
